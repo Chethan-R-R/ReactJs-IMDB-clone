b9057ac1974e793b9db31267aebf7e3a7a7e3f6d
Chethan-R-R <chethan9980rr@gmail.com>
2023-07-10 13:48:51 +0530

    updated dvd

--- src/components/header/Headerm.js
+++ src/components/header/Headerm.js
@@ -25,8 +25,8 @@ const Headerm=(props)=>{
            <Link to={activatelinks[0]}><Dvd activate={activate} id={0} class="dvd1m" name="Trending Movies" no="01" z={6} imgurl="https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701154083.jpg"/></Link>
            <Link to={activatelinks[1]}><Dvd activate={activate} id={1} class="dvdm" name="Trending Tv Shows" no="02" z={5} imgurl="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/10/A5699-An-Architectural-Review-of-Money-Heist.jpg"/></Link>
            <Link to={activatelinks[2]}><Dvd activate={activate} id={2} class="dvdm" name="Popular Movies" no="03" z={4} imgurl="https://cdn.wallpapersafari.com/73/33/VeBDPA.jpg"/></Link>
-           <Link to={activatelinks[3]}><Dvd activate={activate} id={3} class="dvdm" name="Popular Tv Shows" no="04" z={3} imgurl="https://images.hdqwalls.com/wallpapers/mortal-kombat-x-game-of-thrones-o4.jpg"/></Link>
-           <Link to={activatelinks[4]}><Dvd activate={activate} id={4} class="dvdm" name="Top Rated Movies" no="05" z={2} imgurl="https://images4.alphacoders.com/118/1185748.jpg"/></Link>
+           <Link to={activatelinks[3]}><Dvd activate={activate} id={3} class="dvdm" name="Popular Tv Shows" no="04" z={3} imgurl="https://flxt.tmsimg.com/assets/p20392448_i_h9_aa.jpg"/></Link>
+           <Link to={activatelinks[4]}><Dvd activate={activate} id={4} class="dvdm" name="Top Rated Movies" no="05" z={2} imgurl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaJ6g40NYGeczDzWEtOC4Web4nHg4zy7TvG6M6VF2LQlpbn-i-j94Y5l9Qlb4j2LEUKeA&usqp=CAU"/></Link>
            <Link to={activatelinks[5]}><Dvd activate={activate} id={5} class="dvdm" name="Top Rated Tv Shows" no="06" z={1} imgurl="https://cutewallpaper.org/27/breaking-bad-wallpaper-art/2464521910.jpg"/></Link>
         </div>
     )
